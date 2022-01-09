b4323702f26a9c628610af45df6f86b88e5d60b6
Genglin Liu <44108482+genglinliu@users.noreply.github.com>
2022-01-08 23:47:57 -0800

    some more things to fix

--- src/models/two_layer_CNN.py
+++ src/models/two_layer_CNN.py
@@ -37,18 +37,21 @@ class ConvNet_hybrid(nn.Module):
     def __init__(self): 
         super(ConvNet_hybrid, self).__init__()  
           
-        self.hybrid_conv_1 = Hybrid_Conv2d(3, 16, kernel_size=(16, 3, 3, 3), num_cov=12) 
+        self.hybrid_conv_1 = Hybrid_Conv2d(3, 8, kernel_size=(8, 3, 3, 3), num_cov=12) 
         
         # self.conv1 = nn.Conv2d(3, 16, 3)
-        self.conv2 = nn.Conv2d(16, 32, 3)
+        self.conv2 = nn.Conv2d(8, 8, 3)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.fc1 = nn.Linear(6195200, 128) # with hybrid, parameter number becomes 6195200
+        self.layer2 = nn.Sequential(
+            nn.Linear(1548800, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1) 
+        )
         
     def forward(self, x, cov):
         x = F.relu(self.hybrid_conv_1(x, cov))
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(1, -1) 
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.layer2(x)
         return x
